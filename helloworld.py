d8bb7ea4f2d223ac3e31a35b6b26ef184fd15294
psashu <psashu@umich.edu>
2019-09-06 16:04:23 -0400

    added file

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from random import randint
+x = randint(1, 100)
+y = randint(1, 100)
+
+print("Sashankh")
+print(x)
+print(y)
+res = x + y
+print("Sum = " + str(res))
+print("Average = " + str(res / 2.0))
